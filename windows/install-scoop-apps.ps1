d6e6e171e2aba5357ffef2619ea65e2b5d745183
Henning Storck <mail@henningstorck.com>
2025-05-28 10:34:01 +0200

    Add script to install Scoop apps

---
+++ windows/install-scoop-apps.ps1
@@ -0,0 +1,17 @@
+scoop install git
+
+scoop bucket add extras
+scoop bucket add versions
+scoop bucket add games
+
+scoop install main/7zip
+scoop install extras/vscode
+scoop install extras/googlechrome
+scoop install extras/bitwarden
+scoop install extras/obsidian
+scoop install extras/discord
+scoop install extras/spotify
+scoop install extras/nextcloud
+scoop install versions/steam
+sudo scoop install games/epic-games-launcher
+scoop install games/minecraft
